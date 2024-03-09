3485ed4c0b6d4f38531d2ec64c66222262d36e2c
BuildTools <unconfigured@null.spigotmc.org>
2024-03-10 02:18:31 +0800

    update sidebar

--- scripts/components/NavigationBar.tsx
+++ scripts/components/NavigationBar.tsx
@@ -1,8 +1,8 @@
 import * as React from 'react';
-import { useState } from 'react';
-import { Link, NavLink } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { Link, NavLink, useLocation } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCogs, faLayerGroup, faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
+import { faBars, faCogs, faLayerGroup, faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
 import { useStoreState } from 'easy-peasy';
 import { ApplicationStore } from '@/state';
 import SearchContainer from '@/components/dashboard/search/SearchContainer';
@@ -31,11 +31,27 @@ const RightNavigation = styled.div`
         }
     }
 `;
+const onTriggerNavButton = () => {
+    const sidebar = document.getElementById('sidebar');
 
+    if (sidebar) {
+        sidebar.classList.toggle('active-nav');
+    }
+};
 export default () => {
     const name = useStoreState((state: ApplicationStore) => state.settings.data!.name);
     const rootAdmin = useStoreState((state: ApplicationStore) => state.user.data!.rootAdmin);
     const [isLoggingOut, setIsLoggingOut] = useState(false);
+    const location = useLocation();
+    const [showSidebar, setShowSidebar] = useState(false);
+
+    useEffect(() => {
+        if (location.pathname.startsWith('/server') || location.pathname.startsWith('/account')) {
+            setShowSidebar(true);
+            return;
+        }
+        setShowSidebar(false);
+    }, [location.pathname]);
 
     const onTriggerLogout = () => {
         setIsLoggingOut(true);
@@ -49,6 +65,13 @@ export default () => {
         <div className={'w-full bg-neutral-900 shadow-md overflow-x-auto'}>
             <SpinnerOverlay visible={isLoggingOut} />
             <div className={'mx-auto w-full flex items-center h-[3.5rem] max-w-[1200px]'}>
+                {showSidebar && (
+                    <FontAwesomeIcon
+                        icon={faBars}
+                        className='navbar-button'
+                        onClick={onTriggerNavButton}
+                    ></FontAwesomeIcon>
+                )}
                 <div id={'logo'} className={'flex-1'}>
                     <Link
                         to={'/'}
